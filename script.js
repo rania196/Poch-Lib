c3d9b5d4c31099c68cc8914a211b60ee96364b07
rania idaou <rania.idaoumahmoud@gmail.com>
2020-12-29 19:50:15 +0100

    Ajout du boutton "ajouter un livre"

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.createElement("BUTTON");
+const t = document.createTextNode("Ajouter un livre");
+btn.appendChild(t);
+document.getElementById("myBooks").insertBefore(btn,document.getElementById)
